5751051fcd4a386a300255f99a78c82f274a2fcc
juyeongkyeong <juyk.oung@gmail.com>
2025-12-24 15:10:51 +0900

    0016

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,13 +1,14 @@
 package com.back.boundedContext.post.domain;
 
 import com.back.global.jpa.BaseIdAndTime;
+import com.back.global.jpa.BaseIdAndTimeManual;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "POST_MEMBER")
-public class PostMember extends BaseIdAndTime {
+public class PostMember extends BaseIdAndTimeManual {
     @Column(unique = true)
     private String username;
     private String password;
