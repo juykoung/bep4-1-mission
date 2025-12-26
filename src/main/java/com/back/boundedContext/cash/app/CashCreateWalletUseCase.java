83b557db22c1ce06728d602308206583e665a257
juyeongkyeong <juyk.oung@gmail.com>
2025-12-26 17:29:06 +0900

    0026 재반영

--- src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
+++ src/main/java/com/back/boundedContext/cash/app/CashCreateWalletUseCase.java
@@ -1,21 +1,21 @@
-//package com.back.boundedContext.cash.app;
-//
-//import com.back.boundedContext.cash.domain.CashMember;
-//import com.back.boundedContext.cash.domain.Wallet;
-//import com.back.boundedContext.cash.out.WalletRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CashCreateWalletUseCase {
-//    private final WalletRepository walletRepository;
-//
-//    @Transactional
-//    public Wallet createWallet(CashMember holder) {
-//        Wallet wallet = new Wallet(holder);
-//        return walletRepository.save(wallet);
-//    }
-//}
+package com.back.boundedContext.cash.app;
+
+import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.Wallet;
+import com.back.boundedContext.cash.out.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+public class CashCreateWalletUseCase {
+    private final WalletRepository walletRepository;
+
+    @Transactional
+    public Wallet createWallet(CashMember holder) {
+        Wallet wallet = new Wallet(holder);
+        return walletRepository.save(wallet);
+    }
+}
